e2cacd8475bfa5080bd511e29349b3f8afa3dd8e
mrjacobsullivan <mrjacobsullivan@gmail.com>
2021-10-05 14:42:59 -0500

    implemented comment section

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -3,11 +3,14 @@ import Comment from './Comment'
 import './Comments.css'
 
 const Comments = (props) => {
-  // 🔥 Make sure the parent of Comments is passing the right props!
   const { comments } = props
 
   return (
-    <div>{/* map through the comments prop and render a Comment for every piece of data */}</div>
+    <div>
+      {comments.map((comment) => (
+        <Comment comment={comment} key={comment.id} />
+      ))}
+    </div>
   )
 }
 
