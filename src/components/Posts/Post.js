43386a2e345379cdd7663bf323c2ce68a0e01c2f
mrjacobsullivan <mrjacobsullivan@gmail.com>
2021-10-05 14:38:56 -0500

    added like button functionality

--- src/components/Posts/Post.js
+++ src/components/Posts/Post.js
@@ -13,7 +13,7 @@ const Post = (props) => {
         <img alt='post thumbnail' className='post-image' src={post.imageUrl} />
       </div>
       {/* Is LikeSection getting all the props it needs to work correctly? */}
-      <LikeSection likePost={likePost} />
+      <LikeSection likePost={likePost} post={post} />
       {/* Comments also wants its props! */}
       <Comments comments={post.comments} />
     </div>
